4f9e3e74bf055e1f340c97cd29459e257d2baa2e
Othman Alibrahim <acerxr@gmail.com>
2025-07-09 09:19:14 +0300

    setup trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { caller } from "@/trpc/server";
+
 export default async function Home() {
+  const data = await caller.hello({ text: "test" });
+
   return (
     <div>
       <h1>Hello World</h1>
+      <p>{data?.greeting}</p>
     </div>
   );
 }
