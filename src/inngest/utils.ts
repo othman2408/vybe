0012a8ff1f7b1c19c24b60c4f90b6e4d0b2116b5
Othman <acerxr@gmail.com>
2025-07-13 00:55:56 +0300

    create agent tools

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,6 +1,26 @@
 import { Sandbox } from "@e2b/code-interpreter";
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
 export async function getSandbox(id: string) {
   const sandbox = await Sandbox.connect(id);
   return sandbox;
+}
+
+export function lastAssistantTextMessageContent(result: AgentResult) {
+  const lastAssistantTextMessageIdx = result.output.findIndex(
+    (message) => message.role === "assistant"
+  );
+
+  const message = result.output[lastAssistantTextMessageIdx] as
+    | TextMessage
+    | undefined;
+
+  if (!message?.content) {
+    return undefined;
+  }
+  if (typeof message.content === "string") {
+    return message.content;
+  } else {
+    return message.content.map((part) => part.text).join("");
+  }
 }
